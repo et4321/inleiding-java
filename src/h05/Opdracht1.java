8f81e3b462c2885f2686ebf66f05148e02ebbcb5
et4321 <54795466+et4321@users.noreply.github.com>
2019-09-06 12:17:14 +0200

    h5 op1 toegevoegd

--- src/h05/Opdracht1.java
+++ src/h05/Opdracht1.java
@@ -4,17 +4,59 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class Opdracht1 extends Applet {
+    //declaratie.
+    Color opvulkleur;
+    Color lijnkleur;
+    int breedte;
+    int hoogte;
 
-    //init
-    public void init() { setBackground(Color.white); }
+    public void init() {
+        //initialisatie.
+        opvulkleur = Color.MAGENTA;
+        lijnkleur = Color.BLACK;
+        breedte = 150;
+        hoogte = 60;}
 
 
-    //paint
+        //paint
+
     public void paint(Graphics g) {
+        //lijn
+        g.drawLine(10, 50, 150, 50);
+        g.drawString("Lijn", 40, 70);
+
+        //rechthoek
+        g.drawRect(10, 80, breedte, hoogte);
+        g.drawString("Rechthoek", 40, 160);
+
+        //afgeronden rechthoek
+        g.drawRoundRect(10, 180,breedte,hoogte, 10, 10);
+        g.drawString("Afgeronde rechthoek", 35, 260);
+
+        //gevulden rechthoek met ovaal
+        g.setColor(Color.magenta);
+        g.fillRect(180, 80, breedte, hoogte);
         g.setColor(Color.black);
+        g.drawOval(180, 80, breedte,hoogte);
+        g.drawString("Gevulden rechthoek met ovaal", 180, 160);
 
+        //gevulden ovaal
+        g.setColor(Color.magenta);
+        g.fillOval(180, 180,breedte,hoogte );
+        g.setColor(Color.black);
+        g.drawString("Gevulden ovaal", 180, 260);
 
+        //taartpunt met ovaal eromheen
+        g.drawOval(350, 80,breedte,hoogte);
+        g.setColor(Color.magenta);
+        g.fillArc(351, 80, 150, 65, 0, 45);
+        g.setColor(Color.black);
+        g.drawString("Taartpunt met ovaal eromheen", 360, 160);
 
+        //cirkel
+        g.drawOval(400, 180, 60, 60);
+        g.drawString("Cirkel", 400, 260);
 
     }
+
 }
---
+++ src/h05/Opdracht2.java
@@ -0,0 +1,7 @@
+package h05;
+
+import java.applet.Applet;
+
+public class Opdracht2 extends Applet {
+    
+}
