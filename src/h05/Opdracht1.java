8049b8d6e5ba65fb2739575b51f8bf2aa5c8250f
et4321 <54795466+et4321@users.noreply.github.com>
2019-09-06 11:21:18 +0200

    h4 toegevoegd

---
+++ src/h05/Opdracht1.java
@@ -0,0 +1,20 @@
+package h05;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht1 extends Applet {
+
+    //init
+    public void init() { setBackground(Color.white); }
+
+
+    //paint
+    public void paint(Graphics g) {
+        g.setColor(Color.black);
+
+
+
+
+    }
+}
